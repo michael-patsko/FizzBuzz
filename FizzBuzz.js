1c7785aef685ce670c409f40b3e75559893644bf
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-26 13:48:49 +0100

    code now prints only Bong when number is a multiple of 11

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,5 +3,6 @@ for (let i = 1; i <= 100; i++) {
     if (i%3 === 0) output = output.concat("Fizz");
     if (i%5 === 0) output = output.concat("Buzz");    
     if (i%7 === 0) output = output.concat("Bang"); 
+    if (i%11 ===0) output = "Bong";
     output === "" ? console.log(i) : console.log(output);
 }
