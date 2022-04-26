ff5ed60919b72b247111552debb3f4fb891d7908
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-26 11:36:10 +0100

    modified code to make it tidier

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,6 @@
 for (let i = 1; i <= 100; i++) {
     var output = "";
-    if (i%3 != 0 && i%5 != 0) {
-        console.log(i);
-        continue
-    }
     if (i%3 == 0) output = output.concat("Fizz");
-    if (i%5 == 0) output = output.concat("Buzz");
-    console.log(output);
+    if (i%5 == 0) output = output.concat("Buzz");    
+    i%3 != 0 && i%5 != 0 ? console.log(i) : console.log(output);
 }
