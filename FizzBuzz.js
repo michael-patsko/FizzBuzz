1eae4179f058d8dd99b7014f422adf0905d60ac5
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-26 11:10:29 +0100

    code now prints Fizz when number is a multiple of 3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,5 @@
-for (let i = 1; i <= 100; i++) { // This for loop prints every number from 1 to 100
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+    var output = ""; // Creates a variable 'output' which is used for outputting FizzBuzz
+    if (i%3 != 0) console.log(i);
+    if (i%3 == 0) console.log(output.concat("Fizz"));
 }
