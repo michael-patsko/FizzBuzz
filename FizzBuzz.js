8a9c4590d3934589c9e9e5b687a4cff0543c32d2
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-26 13:39:44 +0100

    code now prints Bang when number is a multiple of 7

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,5 +2,6 @@ for (let i = 1; i <= 100; i++) {
     let output = "";
     if (i%3 === 0) output = output.concat("Fizz");
     if (i%5 === 0) output = output.concat("Buzz");    
+    if (i%7 === 0) output = output.concat("Bang"); 
     output === "" ? console.log(i) : console.log(output);
 }
