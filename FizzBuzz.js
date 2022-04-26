d80769d16513c6f7f5efbc610e3ba2ad4a4eea2f
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-26 11:23:54 +0100

    code now prints buzz on multiples of 5 and FizzBuzz on multiples of 3 and 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,10 @@
 for (let i = 1; i <= 100; i++) {
-    var output = ""; // Creates a variable 'output' which is used for outputting FizzBuzz
-    if (i%3 != 0) console.log(i);
-    if (i%3 == 0) console.log(output.concat("Fizz"));
+    var output = "";
+    if (i%3 != 0 && i%5 != 0) {
+        console.log(i);
+        continue
+    }
+    if (i%3 == 0) output = output.concat("Fizz");
+    if (i%5 == 0) output = output.concat("Buzz");
+    console.log(output);
 }
